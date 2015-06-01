b1f3c10e64f92bd9a05f5e19be0b45749cc131c7
Joey Gracey <joeygracey@gmail.com>
2015-06-01 14:32:14 -0700

    asd f

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/data',function(req,res){
 	})
 })
 
-var server = app.listen(3010,function(){
+var server = app.listen(process.env.PORT || 3010,function(){
 	var host = server.address().address;
 	var port = server.address().port;
 	console.log("Listening at http://%s:%s",host,port);
